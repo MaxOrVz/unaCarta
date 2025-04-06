d5d547ae7a88076ed80eb420d9ac4d00004024ff
SoMch <134052153+MaxOrVz@users.noreply.github.com>
2025-04-06 15:41:47 -0600

    Add files via upload

---
+++ ma1s1/script.js
@@ -0,0 +1,9 @@
+const miBoton = document.getElementById('a13331');
+const caja = document.querySelector('.ELIMNA1');
+
+
+miBoton.addEventListener('click', function() {
+    // Aquí va la acción que quieres que ocurra cuando el botón sea presionado
+    caja.classList.toggle('an1none');
+  });
+
